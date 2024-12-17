3e14722867b1d366f9f6aa871185c287700389e8
Seth Bowman <brandonsethbowman@gmail.com>
2024-12-17 15:21:40 -0600

    demo completed

---
+++ EncapsulationDemoCSharp-52/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace EncapsulationDemoCSharp_52;
+
+public class Person
+{
+    //Properties
+    public string Name { get; set; }
+    public int Age { get; set; }
+    
+    //Fields
+    private double _wallet;
+    
+    //Methods (public) can access the private field
+    public void AddMoney(double amount)
+    {
+        _wallet += amount;
+    }
+
+    public double GetWallet()
+    {
+        return _wallet;
+    }
+}
---
+++ EncapsulationDemoCSharp-52/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EncapsulationDemoCSharp_52;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var seth = new Person();
+        
+        var currentWallet = seth.GetWallet();
+
+        Console.WriteLine(currentWallet);
+        
+        seth.AddMoney(500.00);
+        
+        currentWallet = seth.GetWallet();
+
+        Console.WriteLine(currentWallet);
+    }
+}
